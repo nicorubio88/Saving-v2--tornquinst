90a1befd0b81d26ea54fcb8b8a9a0ed8f1d46cb5
nicorubio88 <93954145+nicorubio88@users.noreply.github.com>
2026-07-30 13:50:37 -0300

    Delete frontend directory

--- frontend/js/config.js
+++
@@ -1,3 +0,0 @@
-// Pegar acá la URL del Web App después de "Implementar > Nueva implementación"
-// en el editor de Apps Script. Termina en /exec
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTLH-izy9qL-Y0QXXQEDFg17tpuvncHyQOWeEInayOP4rYUsmj-0jWtjX5kbmFejrJ/exec";
